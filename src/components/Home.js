4219cea597a636664e2acac50c81ad00fe67f577
James Sinker <j@Jamess-MacBook-Pro.local>
2019-12-11 15:03:14 -0800

    stage 1 done

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Home = () => {
+  return(
+    <h2>
+      I am the Home page
+    </h2>
+  );
+}
+
+export default Home;
---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+  render() {
+    return (
+      <h2>
+        I am the todos container page 
+      </h2>
+    );
+  };
+};
+
+export default TodosContainer;
+
