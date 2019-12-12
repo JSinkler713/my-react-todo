1983967232e249c0e2bd5e8ff3e75f5c8222612f
James Sinker <j@Jamess-MacBook-Pro.local>
2019-12-11 21:24:08 -0800

    the rest of sprint 4

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,5 +1,5 @@
-
 import React, { Component } from 'react';
+import CreateTodoForm from '../components/CreateTodoForm.js'
 import TodoModel from '../models/Todo';
 import Todos from '../components/Todos';
 
@@ -21,10 +21,26 @@ class TodosContainer extends Component {
     });
   };
 
+  //passing state up the component from below in the CreateTodoForm
+  createTodo = (todo) => {
+    let newTodo = {
+      body: todo,
+      completed: false
+    };
+
+    TodoModel.create(newTodo).then((res)=> {
+      let todos = this.state.todos;
+      todos.push(res);
+      this.setState({ todos: todos });
+    });
+  };
 
   render() {
     return (
-      <div className='todosContainer'>
+      <div className='todosComponent'>
+        <CreateTodoForm
+          createTodo={this.createTodo} />
+
         <Todos todos={this.state.todos} />
       </div>
     );
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -6,6 +6,17 @@ class TodoModel {
       .then(response => response.json())
       .catch(err => console.log('Could not get all todos\n', err));
   };
+  static create = (todo) => {
+    return fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(todo)
+    })
+      .then(response => response.json())
+      .catch(error => console.log('Could not post todo \n', error))
+  }
 };
 
 export default TodoModel
